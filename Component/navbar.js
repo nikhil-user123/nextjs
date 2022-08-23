cf0ccfcb2529dfa136f7fc23ca6eeb811d512522
nikhil-user123 <nikhilprajapat@gmail.com>
2022-08-23 13:30:35 +0530

    nav updated

--- Component/navbar.js
+++ Component/navbar.js
@@ -1,18 +1,35 @@
-import Link from "next/link";
+import Link from"next/link";
 import styles from '../styles/index.module.css';
- const Navbar = () => { 
-   return ( 
-     <> 
-       <nav className={styles.nav}>
-           <ul className={styles.ul}>
-            <li><a className={styles.text} href="#">Abstract</a></li>
-            <li><p className={styles.text} href="#">|</p></li>
-            <li><a className={styles.text} href="#">HelpCenter</a></li>
-           </ul>
-             <button  className={styles.button_sign}>Sign in</button>
-       </nav>
-     </> 
-   ); 
+ const Navbar=()=>{ 
+        return( 
+                <> 
+                <nav className={styles.nav}>
+                <ul className={styles.ul}>
+                <li className={styles.li}> 
+                    <Link href="/" className={styles.text} >Astract</Link>
+                </li>
+                <li className={styles.li}> 
+                    <p>|</p>
+                </li>
+                <li className={styles.li}> 
+                    <Link href="/" className={styles.text} >HelpCenter</Link>
+                </li>    
+                </ul>
+                <input type="checkbox" name="click" id="click">
+
+                <label for="" className={styles.search}>
+                <i className={fa fa-search} aria-hidden="true"></i>    
+                </label>
+
+                
+
+                <div className={styles.button}>
+                <button  className={styles.button_Sub}>Submit a Request</button>
+                <button  className={styles.button_sign}>Sign in</button>
+                </div>
+           </nav>
+    </> 
+ ); 
  }; 
   
- export default Navbar;
+ export default Navbar;
